0864add4931aa252e2fee8bd45d0951f024ab640
josuec4321 <josuec4321@gmail.com>
2020-05-24 21:10:03 -0500

    Creacion parcial del espacio de trabajo

---
+++ SISGED/Shared/Models/Tool.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SISGED.Shared.Models
+{
+    public class Tool
+    {
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public string icono { get; set; }
+        //public List<string> roles { get; set; } = new List<string>();
+        /*posibles lugares 'tools' o 'workspace'*/
+        public string currentPlace { get; set; } = "tools";
+        public string componentName { get; set; } = "ContenidoPrueba";
+    }
+}
