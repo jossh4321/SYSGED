fedc7db2d333b2268a3f71ceb3b2076811ea3598
josuec4321 <josuec4321@gmail.com>
2020-06-07 03:36:29 -0500

    avance parcial de autocomplete de asignacion de notario

---
+++ SISGED/Server/Services/NotarioService.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Driver;
+using SISGED.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SISGED.Server.Services
+{
+    public class NotarioService
+    {
+        private readonly IMongoCollection<Notario> _notarios;
+
+        public NotarioService(ISysgedDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _notarios = database.GetCollection<Notario>("notarios");
+        }
+
+        /*public List<Notario> Get()
+        {
+            return _personas.Find(persona => true).ToList();
+        }*/
+
+    }
+}
