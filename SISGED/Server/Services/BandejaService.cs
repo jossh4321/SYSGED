43ac86c9b9c2572c90ff30d763bbdb737c652ec1
sebasmm12 <smirandamanrique@gmail.com>
2020-06-14 01:57:27 -0500

    Subida del 40% de la creación de la bandeja de salida

---
+++ SISGED/Server/Services/BandejaService.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Driver;
+using SISGED.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SISGED.Server.Services
+{
+    public class BandejaService
+    {
+        private readonly IMongoCollection<Bandeja> _bandejas;
+
+        public BandejaService(ISysgedDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            _bandejas = database.GetCollection<Bandeja>("bandejas");
+        }
+
+        /*public Bandeja ObtenerBandejaDocumento()
+        {
+           
+        }*/
+    }
+}
