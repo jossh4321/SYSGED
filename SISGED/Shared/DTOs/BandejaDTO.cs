af9fc24a66f2d363780d839c8583c2deb6f53254
sebasmm12 <smirandamanrique@gmail.com>
2020-06-15 00:20:01 -0500

    Subida del 37% de la bandeja de salida

---
+++ SISGED/Shared/DTOs/BandejaDTO.cs
@@ -0,0 +1,26 @@
+﻿using SISGED.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SISGED.Shared.DTOs
+{
+    public class BandejaDTO
+    {
+        public string id { get; set; }
+        public string tipo { get; set; }
+        public string usuario { get; set; }
+        public List<BandejaDTODocumento> bandejaentrada { get; set; }
+        public List<BandejaDTODocumento> bandejasalida { get; set; }
+    }
+
+    public class BandejaDTODocumento
+    {
+        public string idexpediente { get; set; }
+        public string iddocumento { get; set; }
+        public string nombreexpediente { get; set; }
+        public string tipoDocumento { get; set; }
+        public string nombreCliente { get; set; }
+
+    }
+}
