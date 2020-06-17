d6af89c7fedd3beb71c7eafef08b497dd6dee10f
piero lavado cervantes <piero_lvdo_c@hotmail.com>
2020-06-16 20:21:01 -0500

    Cambios documentos -Expediente wrapper

--- SISGED/Server/Services/DocumentoService.cs
+++ SISGED/Server/Services/DocumentoService.cs
@@ -86,11 +86,63 @@ namespace SISGED.Server.Services
             _bandejas.UpdateOne(band => band.usuario == "5ee2e87bd12fcd05d6b6b13e", updateBandeja);
             return documentoODN;
         }
-        public OficioBPN registrarSolicitudBPN(OficioBPN documentoOficioBPN)
+        public OficioBPN registrarOficioBPNE(ExpedienteWrapper expedienteWrapper)
+        {
+            //Obtenemos los datos del expedientewrapper
+            OficioBPNDTO oficioBPNDTO = new OficioBPNDTO();
+            var json = JsonConvert.SerializeObject(expedienteWrapper.documento);
+            oficioBPNDTO = JsonConvert.DeserializeObject<OficioBPNDTO>(json);
+
+            //Insertando el oficio normal
+            ContenidoOficioBPN contenidoSolicitudBPN = new ContenidoOficioBPN()
+            {
+                titulo = oficioBPNDTO.contenidoDTO.titulo,
+                descripcion = oficioBPNDTO.contenidoDTO.descripcion,
+                observacion = oficioBPNDTO.contenidoDTO.observacion,
+                idcliente = oficioBPNDTO.contenidoDTO.idcliente.id,
+                direccionoficio = oficioBPNDTO.contenidoDTO.direccionoficio,
+                idnotario = oficioBPNDTO.contenidoDTO.idnotario.id,
+                actojuridico = oficioBPNDTO.contenidoDTO.actojuridico,
+                tipoprotocolo = oficioBPNDTO.contenidoDTO.tipoprotocolo,
+                otorgantes = oficioBPNDTO.contenidoDTO.otorgantes,
+                fecharealizacion = DateTime.Now,
+                url = "ninguna"
+            };
+            OficioBPN documentoBPN = new OficioBPN()
+            {
+                tipo = "OficioBPN",
+                contenido = contenidoSolicitudBPN,
+                estado = "pendiente",
+                historialcontenido = new List<ContenidoVersion>(),
+                historialproceso = new List<Proceso>()
+            };
+            _documentos.InsertOne(documentoBPN);
+
+            //Actualizamos el expediente y agregamos el documento a sus documentos contenidos
+            DocumentoExpediente documentoExpediente = new DocumentoExpediente();
+            documentoExpediente.indice = 2;
+            documentoExpediente.iddocumento = documentoBPN.id;
+            documentoExpediente.tipo = "OficioBPN";
+            documentoExpediente.fechacreacion = DateTime.Now;
+            documentoExpediente.fechaexceso = DateTime.Now.AddDays(5);
+            documentoExpediente.fechademora = null;
+
+            UpdateDefinition<Expediente> updateExpediente = Builders<Expediente>.Update.Push("documentos", documentoExpediente);
+            Expediente expediente = _expedientes.FindOneAndUpdate(x => x.id == "5ee5f24e7d8f833f68cc88a0", updateExpediente);
+
+            BandejaDocumento bandejaDocumento = new BandejaDocumento();
+            bandejaDocumento.idexpediente = expediente.id;
+            bandejaDocumento.iddocumento = documentoExpediente.iddocumento;
+            UpdateDefinition<Bandeja> updateBandeja = Builders<Bandeja>.Update.Push("bandejasalida", bandejaDocumento);
+            _bandejas.UpdateOne(band => band.usuario == "5ee2e87bd12fcd05d6b6b13e", updateBandeja);
+            return documentoBPN;
+        }
+        /*Esto funciona
+         * public OficioBPN registrarOficioBPN(OficioBPN documentoOficioBPN)
         {
             _documentos.InsertOne(documentoOficioBPN);
             return documentoOficioBPN;
-        }
+        }*/
         public SolicitudExpedicionFirma registrarSolicitudExpedicionFirma(SolicitudExpedicionFirma documentoSEF)
         {
             _documentos.InsertOne(documentoSEF);
@@ -101,6 +153,55 @@ namespace SISGED.Server.Services
             _documentos.InsertOne(documentoSD);
             return documentoSD;
         }
+        public ConclusionFirma registrarConclusionFirmaE(ExpedienteWrapper expedienteWrapper)
+        {
+            //Obtenemos los datos del expedientewrapper
+            ConclusionFirmaDTO conclusionfirmaDTO = new ConclusionFirmaDTO();
+            var json = JsonConvert.SerializeObject(expedienteWrapper.documento);
+            conclusionfirmaDTO = JsonConvert.DeserializeObject<ConclusionFirmaDTO>(json);
+
+            //Insertando la conclusion normal
+            ContenidoConclusionFirma contenidoCF = new ContenidoConclusionFirma()
+            {
+                idescriturapublica = conclusionfirmaDTO.contenidoDTO.idescriturapublica.id
+            };
+            ConclusionFirma documentoDF = new ConclusionFirma()
+            {
+                tipo = "ConclusionFirma",
+                contenido = contenidoCF,
+                estado = "pendiente",
+                historialcontenido = new List<ContenidoVersion>(),
+                historialproceso = new List<Proceso>()
+            };
+            _documentos.InsertOne(documentoDF);
+
+            //Actualizamos el expediente y agregamos el documento a sus documentos contenidos
+            DocumentoExpediente documentoExpediente = new DocumentoExpediente();
+            documentoExpediente.indice = 8;
+            documentoExpediente.iddocumento = documentoDF.id;
+            documentoExpediente.tipo = "ConclusionFirma";
+            documentoExpediente.fechacreacion = DateTime.Now;
+            documentoExpediente.fechaexceso = DateTime.Now.AddDays(5);
+            documentoExpediente.fechademora = null;
+
+            UpdateDefinition<Expediente> updateExpediente = Builders<Expediente>.Update.Push("documentos", documentoExpediente);
+            Expediente expediente = _expedientes.FindOneAndUpdate(x => x.id == "5ee5f24e7d8f833f68cc88a0", updateExpediente);
+
+            BandejaDocumento bandejaDocumento = new BandejaDocumento();
+            bandejaDocumento.idexpediente = expediente.id;
+            bandejaDocumento.iddocumento = documentoExpediente.iddocumento;
+            UpdateDefinition<Bandeja> updateBandeja = Builders<Bandeja>.Update.Push("bandejasalida", bandejaDocumento);
+            _bandejas.UpdateOne(band => band.usuario == "5ee2e87bd12fcd05d6b6b13e", updateBandeja);
+            return documentoDF;
+        }
+
+        /*Esto funciona
+         * public ConclusionFirma registrarConclusionFirma(ConclusionFirma documentoCF)
+        {
+            _documentos.InsertOne(documentoCF);
+            return documentoCF;
+        }/*
+
         /*public Documento modificarEstado(Documento documento)
         {
             var filter = Builders<Documento>.Filter.Eq("id", documento.id);
