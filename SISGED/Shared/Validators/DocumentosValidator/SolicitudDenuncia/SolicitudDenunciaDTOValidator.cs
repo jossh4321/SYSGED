a7e1f6304c2abf573c4be02b74402d17fe1f492e
piero lavado cervantes <piero_lvdo_c@hotmail.com>
2020-06-18 01:43:59 -0500

    Arreglos - validacion

--- SISGED/Shared/Validators/DocumentosValidator/SolicitudDenuncia/SolicitudDenunciaDTOValidator.cs
+++ SISGED/Shared/Validators/DocumentosValidator/SolicitudDenuncia/SolicitudDenunciaDTOValidator.cs
@@ -3,6 +3,7 @@ using SISGED.Shared.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace SISGED.Shared.Validators.DocumentosValidator.SolicitudDenuncia
 {
@@ -11,8 +12,9 @@ namespace SISGED.Shared.Validators.DocumentosValidator.SolicitudDenuncia
         public SolicitudDenunciaDTOValidator()
         {
             RuleFor(x => x.nombrecliente).NotEmpty().WithMessage("Debe Ingresar un cliente obligatoriamente");
-            RuleFor(x => x.tipodocumento).NotEmpty().WithMessage("Debe Ingresar un tipo de dcumento obligatoriamente");
+            RuleFor(x => x.tipodocumento).NotEmpty().WithMessage("Debe Ingresar un tipo de documento obligatoriamente");
             RuleFor(x => x.numerodocumento).NotEmpty().WithMessage("Debe Ingresar un numero de Documento obligatoriamente");
+            RuleFor(x => x.numerodocumento).Matches(@"^[0-9]{8}$").WithMessage("El número de documento esta mal escrito").When(x => x.tipodocumento == "DNI");
             RuleFor(x => x.contenidoDTO).SetValidator(new ContenidoSolicitudDenunciaDTOValidator());
         }
     }
--- SISGED/Shared/Validators/DocumentosValidator/SolicitudExpedicionFirmaValidator.cs
+++ SISGED/Shared/Validators/DocumentosValidator/SolicitudExpedicionFirmaValidator.cs
@@ -4,6 +4,7 @@ using SISGED.Shared.Entities;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace SISGED.Shared.Validators.DocumentosValidator.DesignacionNotario
 {
@@ -12,8 +13,9 @@ namespace SISGED.Shared.Validators.DocumentosValidator.DesignacionNotario
         public SolicitudExpedicionFirmaValidator()
         {
             RuleFor(x => x.nombrecliente).NotEmpty().WithMessage("Debe Ingresar un cliente obligatoriamente");
-            RuleFor(x => x.tipodocumento).NotEmpty().WithMessage("Debe Ingresar un tipo de dcumento obligatoriamente");
+            RuleFor(x => x.tipodocumento).NotEmpty().WithMessage("Debe Ingresar un tipo de documento obligatoriamente");
             RuleFor(x => x.numerodocumento).NotEmpty().WithMessage("Debe Ingresar un numero de Documento obligatoriamente");
+            RuleFor(x => x.numerodocumento).Matches(@"^[0-9]{8}$").WithMessage("El número de documento esta mal escrito").When(x => x.tipodocumento == "DNI");
             RuleFor(x => x.contenidoDTO).SetValidator(new ContenidoSEFValidator());
         }
     }
