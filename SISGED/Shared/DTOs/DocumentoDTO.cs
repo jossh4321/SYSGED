1c1ef9e88be2e899b1dac62362e7de235cd8b316
sebasmm12 <smirandamanrique@gmail.com>
2020-06-18 06:37:21 -0500

    Cambio del DocumentoDTO

--- SISGED/Shared/DTOs/DocumentoDTO.cs
+++ SISGED/Shared/DTOs/DocumentoDTO.cs
@@ -1,16 +1,29 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using SISGED.Shared.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SISGED.Shared.DTOs
 {
-    class DocumentoDTO
-    {
-    }
+   
 
     public class OpcionDocumento
     {
         public string label { get; set; } = "";
         public string value { get; set; } = "";
+    }
+
+    public class DocumentoDTO
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
+        public string tipo { get; set; }
+        public List<ContenidoVersion> historialcontenido { get; set; } = new List<ContenidoVersion>();
+        public List<Proceso> historialproceso { get; set; } = new List<Proceso>();
+        public Object contenido { get; set; }
+        public Object estado { get; set; }
     }
 }
