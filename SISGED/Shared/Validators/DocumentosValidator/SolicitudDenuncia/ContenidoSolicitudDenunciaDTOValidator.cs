adfab342ff12a66f9bf3811019dae1d14d0f8e7e
josuec4321 <josuec4321@gmail.com>
2020-06-22 20:02:41 -0500

    commit segunda iteracion

--- SISGED/Shared/Validators/DocumentosValidator/SolicitudDenuncia/ContenidoSolicitudDenunciaDTOValidator.cs
+++ SISGED/Shared/Validators/DocumentosValidator/SolicitudDenuncia/ContenidoSolicitudDenunciaDTOValidator.cs
@@ -13,7 +13,7 @@ namespace SISGED.Shared.Validators.DocumentosValidator.SolicitudDenuncia
             RuleFor(x => x.codigo).NotEmpty().WithMessage("Debe Ingresar un Codigo obligatoriamente");
             RuleFor(x => x.titulo).NotEmpty().WithMessage("Debe Ingresar un titulo obligatoriamente");
             RuleFor(x => x.descripcion).NotEmpty().WithMessage("Debe Ingresar una descripcion obligatoriamente");
-            RuleFor(x => x.nombrecliente).NotEmpty().WithMessage("Debe Ingresar un cliente obligatoriamente");
+            //RuleFor(x => x.nombrecliente).NotEmpty().WithMessage("Debe Ingresar un cliente obligatoriamente");
         }
     }
 }
