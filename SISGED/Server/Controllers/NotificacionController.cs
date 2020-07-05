2462ac951fab3ab2a7fae84de388a930e9b944d8
Gloria <gloriandreallerena@gmail.com>
2020-07-05 04:35:52 -0500

    Cambios realizados en las notificaciones

---
+++ SISGED/Server/Controllers/NotificacionController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SISGED.Server.Controllers
+{
+    public class NotificacionController : ControllerBase
+    {
+        private readonly NotificacionService _notificacion;
+
+        public NotificacionController(NotificacionService notificacion)
+        {
+            _notificacion = notificacion;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Notificacion>> Get()
+        {
+            return _notificacion.Get();
+        }
+    }
+}
---
+++ SISGED/Server/Services/NotificacionService.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Driver;
+using SISGED.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SISGED.Server.Services
+{
+    public class NotificacionService
+    {
+        private readonly IMongoCollection<Notificacion> _notificaciones;
+        public NotificacionService(ISysgedDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _notificaciones = database.GetCollection<Notificacion>("notificaciones");
+
+        }
+        public List<Notificacion> Get()
+        {
+            return _notificaciones.Find<Notificacion>(notifiacion => true).ToList();
+        }
+    }
+}
