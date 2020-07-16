97c70427cbbcfe7eb4953302dfcf36be1a9a9712
piero lavado cervantes <piero_lvdo_c@hotmail.com>
2020-07-16 16:08:11 -0500

    Ajustes imputfile nueva version

---
+++ SISGED/Shared/DTOs/ArchivoObtenido.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SISGED.Shared.DTOs
+{
+    public class ArchivoObtenido
+    {
+        public string bytesSeleccionados { get; set; }
+        public string nombreSeleccionado { get; set; }
+    }
+}
