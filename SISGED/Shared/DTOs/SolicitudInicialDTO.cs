db033797826dea3d7dc3d98856f7188cc86525f1
Stefano GL <steph.gl@hotmail.com>
2020-07-26 02:19:12 -0500

    cambios chidos

--- SISGED/Shared/DTOs/SolicitudInicialDTO.cs
+++ SISGED/Shared/DTOs/SolicitudInicialDTO.cs
@@ -7,12 +7,17 @@ namespace SISGED.Shared.DTOs
 {
     public class SolicitudInicialDTO : Documento
     {
+        public string nombrecliente { get; set; }
+        public string tipodocumento { get; set; }
+        public string numerodocumento { get; set; }
         public ContenidoSolicitudInicialDTO contenidoDTO { get; set; } = new ContenidoSolicitudInicialDTO();
     }
 
     public class ContenidoSolicitudInicialDTO
     {
+        public Usuario idcliente { get; set; } = new Usuario();
         public string titulo { get; set; }
         public string descripcion { get; set; }
+        public List<string> Urlanexo { get; set; } = new List<string>();
     }
 }
