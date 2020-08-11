c708736ae52f5935547e7226d85d2f4d52a983fd
Stefano GL <steph.gl@hotmail.com>
2020-08-11 03:41:40 -0500

    color otorgantes y hechos

--- SISGED/Shared/Validators/DocumentosValidator/SolicitudInicial/ContenidoSolicitudInicialDTOValidator.cs
+++ SISGED/Shared/Validators/DocumentosValidator/SolicitudInicial/ContenidoSolicitudInicialDTOValidator.cs
@@ -10,6 +10,8 @@ namespace SISGED.Shared.Validators.DocumentosValidator.SolicitudInicial
     {
         public ContenidoSolicitudInicialDTOValidator()
         {
+            RuleFor(x => x.titulo).Matches(@"^[A-aZ-z0-9ñáéíóú. ]*[A-aZ-z0-9ñáéíóú.]$")
+               .WithMessage("Debe ingresar un titulo válido").When(x => x.titulo != null && x.titulo != "");
             RuleFor(x => x.titulo).NotEmpty().WithMessage("Debe ingresar el titulo de la solicitud inicial");
             RuleFor(x => x.descripcion).NotEmpty().WithMessage("Debe Ingresar la descripción de la solicitud inicial obligatoriamente");
         }
